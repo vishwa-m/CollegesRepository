fb0e76aa6bdc535924622a2306175b1732fd8b64
vishwa-m <vishwanath.manchala@gmail.com>
2016-07-04 23:53:06 +0530

    Updated DepartmentTest

--- test/com/vnath/test/DepartmentTest.java
+++ test/com/vnath/test/DepartmentTest.java
@@ -7,7 +7,7 @@ public class DepartmentTest {
 	public static void main(String[] args) {
 		Department dept = new Department();
 		dept.setdeptID("Dept_001");
-		dept.setdeptName("Electronics and Communication Engineering Dept");
+		dept.setdeptName("Electronics and Communication Engineering Department");
 		dept.showDepartmentDetails(); 
 	}
 }
